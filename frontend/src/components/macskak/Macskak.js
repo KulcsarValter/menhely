da05bcf3bc1424340c0e1621ff8cdb21e8538b2f
KulcsarValter <103348499+KulcsarValter@users.noreply.github.com>
2023-09-11 19:44:23 +0200

    Képek egyforma méretűek maradnak.

--- frontend/src/components/macskak/Macskak.js
+++ frontend/src/components/macskak/Macskak.js
@@ -61,15 +61,20 @@ function Macskak() {
       <div className="d-flex justify-content-center my-5">
         {macskak.map((data, i) => (
           <div className="pa1 kartyak grow ma1 br2 my-5" key={i}>
+            {data.allatkep && (
+              <img
+                src={`http://localhost:3001/${data.allatkep}`}
+                height={200}
+                width={300}
+                alt={data.allatnev}
+              />
+            )}
             <p>Állat neve: {data.allatnev}</p>
             <p>Faj: {data.allatfaj}</p>
             <p>Fajta: {data.allatfajta}</p>
             <p>Neme: {data.allativar}</p>
             <p>Kora: {data.allatkor} éves</p>
             <p>Leírás: {data.allatleiras}</p>
-            {data.allatkep && (
-                <img src={`http://localhost:3001/${data.allatkep}`} alt={data.allatnev} />
-              )}
           </div>
         ))}
       </div>
