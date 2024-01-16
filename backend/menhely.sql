685b8f70acf7bba9458a330ffe2e4dbb09995913
KulcsarValter <kulcsarvalter1975@gmail.com>
2024-01-16 16:44:24 +0100

    Adatbázis frissítve

--- backend/menhely.sql
+++ backend/menhely.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Jan 06. 11:25
+-- Létrehozás ideje: 2024. Jan 16. 16:40
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.2.4
 
@@ -64,8 +64,10 @@ CREATE TABLE `allatok` (
 INSERT INTO `allatok` (`allatid`, `allatfaj`, `allatnev`, `allatkor`, `allatfajta`, `allativar`, `allatstatusz`, `allatleiras`, `allatkep`) VALUES
 (14, 'Kutya', 'Rex', 5, 'Németjuhász', 'Kan', 1, 'Rex, amikor nem fúrja magát éppen az öledbe, kezedbe, akkor megmutatkozik a határtalan huncutsága. Vigyázat, a csokibarna kisfiú függőséget okoz. Nyugodtan lehet tolongani, van vele feladat.', 'public/images/Rex.jpg'),
 (15, 'Macska', 'Cirmi', 1, 'Házimacska', 'Nőstény', 1, 'Barátságos cica. Mintha mindig is ismert volna minket, nem fél, azonnal jön, hogy simogassuk és foglalkozzunk vele. Lakásban érzi magát a legjobban,de a szabadban is feltalálja magát.', 'public/images/Cirmi.jpg'),
-(16, 'Kutya', 'Tyson', 1, 'Rottweiler', 'Kan', 1, 'Tyson kedves, érdeklődő, szereti az emberek társaságát. A simogatást, hűséggel jutalmazza.', 'public/images/allatKep-1694451006908-964839390allatKep-1693937811840-696249611Tyson.jpg'),
-(18, 'Kutya', 'Cimbi', 5, 'Labrador', 'Nöstény', 0, 'Igen ragaszkodó, játékos, szeretet igényes, szófogadó tündéri kutya.\r\nIgazi felelős gazdira vár, ez a szépséges, okos kutyus.', 'public/images/allatKep-1704109251207-607014795Cimbi.jpg');
+(16, 'Kutya', 'Tyson', 1, 'Rottweiler', 'Kan', 1, 'Tyson kedves, érdeklődő, szereti az emberek társaságát. A simogatást, hűséggel jutalmazza.', 'public/images/Tyson.jpg'),
+(19, 'Kutya', 'Cimbi', 0, 'Golden retriver', 'Kan', 0, 'Csodálatos Golden retriever, ivartalanított kan kutya örökbefogadót keres. Kertes házban igazi házörző. Nagyon ragaszkodó, de most még nagyon játékos. Előző örökbefogadója visszaadta, mert a kertjében két eper palántát kikapart. Felelős gazdát keresünk ré', 'public/images/Cimbi.jpg'),
+(20, 'Macska', 'Garfield', 1, 'Házimacska.', 'Nőstény', 0, 'Szerető gazdára vár Garfield, akinek a gazdájának önhibáján kívül el kellett hagynia otthonát és nem tudott a cicájának biztonságos helyet találni.  Szobatiszta, hízelgő, nagyon szelíd cica.', 'public/images/Garfield.jpg'),
+(21, 'Macska', 'Cirmos', 2, 'Házimacska.', 'Kan', 0, 'Szobatiszta, kedves és hízelgő.  Testvére is gazdát keres, szintén ivartalanított kandúr.  Szerető, felelős gazdáikra várnak a menhelyen.', 'public/images/Cirmos.jpg');
 
 -- --------------------------------------------------------
 
@@ -142,7 +144,7 @@ ALTER TABLE `orokbefogadott`
 -- AUTO_INCREMENT a táblához `allatok`
 --
 ALTER TABLE `allatok`
-  MODIFY `allatid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `allatid` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT a táblához `orokbefogado`
